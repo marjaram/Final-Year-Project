068ae8fd1e5183b5748e042d6c0e4f617dbf6175
chrysomallus <lchf56@durham.ac.uk>
2023-04-26 13:18:28 +0100

    results_collection

--- heuristics/data_analysis.py
+++ heuristics/data_analysis.py
@@ -1,11 +1,18 @@
+############################ Experimental Data Collection ########################################################
+
+import sys
 import numpy as np 
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 plt.style.use('_mpl-gallery')
+plt.rcParams.update({'font.size': 12})
+plt.rcParams["figure.autolayout"] = True
 
-dataset = 'training'
+dataset = 'testing'
 df = pd.read_csv(f'low_level_scores_{dataset}_set.csv')
 
+# Full dataframe of heuristics and results the whole dataset
 results_df = df.loc[:, [
          'First Fit Score',
          'First Fit Decreasing Score',
@@ -17,12 +24,25 @@ results_df = df.loc[:, [
          'Djang-Fitch 2 Score',
          ]]
 
-
+# Number of times each heuristic was the best
 best_heuristics = df['Best heuristic'].value_counts()
 best_heuristics.to_csv(f'low_level_best_heuristics_{dataset}.csv')
 
+# Summary of heuristic performances, mean, quartiles etc...
 results_df.describe().to_csv(f'low_level_scores_{dataset}_summary.csv')
 
+# Initial Hyper heuristic scores on the testing set
+hh_scores = pd.read_csv('hyper_heuristic_testing_archive data_(popsize=20,num_iterations=500)_fitness_df_400.csv')
+hh_scores = hh_scores[['0','1','2','3','4']]
+hh_scores.columns = ['HH0','HH1','HH2','HH3','HH4']
+
+# Secondary hyper heuristic scores
+hh2_scores = pd.read_csv('hyper_heuristic_testing_archive data_popsize=40,num_iterations=200_fitness_df_150.csv.csv')
+hh2_scores = hh2_scores[['0','1','2','3','4']]
+hh2_scores.columns = ['HH0','HH1','HH2','HH3','HH4']
+
+
+# Dataframe of time taken by heuristics
 df2 = pd.read_csv(f'low_level_times_{dataset}_set.csv')
 times_df = df2.loc[:, [
          'First Fit Times',
@@ -34,11 +54,51 @@ times_df = df2.loc[:, [
          'Djang-Fitch Times',
          'Djang-Fitch 2 Times']]
 
+# Summary of time data
 times_df.describe().to_csv(f'low_level_times_{dataset}_summary.csv')
 
-plot = False
-if plot:
-    plt.rcParams["figure.figsize"] = [8, 4]
+ff_df = pd.DataFrame({'Fitness': df['First Fit Score'],
+                      'Heuristic': 'First Fit'})
+ffd_df = pd.DataFrame({'Fitness': df['First Fit Decreasing Score'],
+                      'Heuristic': 'First Fit Decreasing'})
+nf_df = pd.DataFrame({'Fitness': df['Next Fit Score'],
+                      'Heuristic': 'Next Fit'})
+nfd_df = pd.DataFrame({'Fitness': df['Next Fit Decreasing Score'],
+                      'Heuristic': 'Next Fit Decreasing'})
+bf_df = pd.DataFrame({'Fitness': df['Best Fit Score'],
+                      'Heuristic': 'Best Fit'})
+bfd_df = pd.DataFrame({'Fitness': df['Best Fit Decreasing Score'],
+                      'Heuristic': 'Best Fit Decreasing'})
+djd_df = pd.DataFrame({'Fitness': df['Djang-Fitch Score'],
+                      'Heuristic': 'Djang and Fitch'})
+djd2_df = pd.DataFrame({'Fitness': df['Djang-Fitch 2 Score'],
+                      'Heuristic': 'Djang and Fitch 2'})
+hh0_df = pd.DataFrame({'Fitness': hh_scores['HH0'],
+                       'Heuristic': 'Converged HH'})
+# hh1_df = pd.DataFrame({'Fitness': hh_scores['HH1'],
+#                        'Heuristic': 'HH1'})
+# hh2_df = pd.DataFrame({'Fitness': hh_scores['HH2'],
+#                        'Heuristic': 'HH2'})
+# hh3_df = pd.DataFrame({'Fitness': hh_scores['HH3'],
+#                        'Heuristic': 'HH3'})
+# hh4_df = pd.DataFrame({'Fitness': hh_scores['HH4'],
+#                        'Heuristic': 'HH4'})
+hh2_scores_df = pd.DataFrame({'Fitness': hh2_scores['HH0'],
+                       'Heuristic': 'Broad HH'})
+
+concat_df = pd.concat([ff_df, ffd_df, nf_df, nfd_df, bf_df, bfd_df, djd_df, djd2_df, hh0_df, hh2_scores_df])
+concat_df.to_csv('concat_df.csv')
+
+results_df = pd.merge(results_df, hh_scores['HH0'], left_index=True, right_index=True)
+results_df = pd.merge(results_df, hh2_scores['HH0'], left_index=True, right_index=True)
+# Plotting
+plot_scatter = False
+plot_box = True
+quartile_plot = False
+
+
+if plot_scatter:
+    plt.rcParams["figure.figsize"] = [8, 6]
     plt.rcParams["figure.autolayout"] = True
 
     plt.scatter(results_df.mean()[0], 0.5, label='FF')
@@ -52,8 +112,36 @@ if plot:
 
     ax = plt.gca()
     ax.get_yaxis().set_visible(False)
-    plt.xlabel('Plot Number')
+    plt.xlabel(f'Mean fitness across {dataset} set')
     plt.legend(loc='upper right')
     manager = plt.get_current_fig_manager()
     manager.full_screen_toggle()
+    plt.savefig('low_level_performance')
+    plt.show()
+
+if plot_box:  
+    plt.rcParams["figure.autolayout"] = True
+    positions = range(len(results_df.columns))
+    plt.boxplot([results_df[col] for col in results_df.columns],
+                positions=positions, showfliers=False,
+                boxprops={'facecolor': 'none'}, medianprops={'color': 'black'}, patch_artist=True)
+    means = [np.mean(results_df[col]) for col in results_df.columns]
+    sns.violinplot('Heuristic', 'Fitness', data=concat_df)
+    plt.show()
+
+
+if quartile_plot:
+    mean_series = results_df.mean(axis=1)
+    quartile_df = results_df.quantile(q=[0.25, 0.5, 0.75], axis=1, numeric_only=True).transpose()
+    quartile_df.columns = ['Lower Quartile', 'Median', 'Upper Quartile']
+    quartile_df['Mean'] = mean_series
+
+    quartile_df = quartile_df[:100]
+
+    quartile_df['Mean'].plot.line()
+    quartile_df['Lower Quartile'].plot.line()
+    quartile_df['Median'].plot.line()
+    quartile_df['Upper Quartile'].plot.line()
+    leg = plt.legend(loc='upper right', frameon=False)
+
     plt.show()
--- heuristics/hyperheuristic_analysis.py
+++ heuristics/hyperheuristic_analysis.py
@@ -1,21 +1,15 @@
-##################################################### IMPORTS #######################################################
-import math
 import sys
 import numpy as np
 import pandas as pd
-import ast
-import logging
-import placement
 import copy
-seed = np.random.default_rng(42)
-logger = logging.getLogger('my_module_name')
-logging.basicConfig(filename='log.log',filemode='w', encoding='utf-8', level=logging.DEBUG)
+import placement
+import logging
+import ast
+seed = np.random.default_rng(52)
 
-training_set = pd.read_csv('training_set.csv')['Item sets']
-training_set = training_set.apply(lambda x: ast.literal_eval(x))
-
-testing_set = pd.read_csv('testing_set.csv')['Item sets']
-
+testing_set_df = pd.read_csv('testing_set.csv')
+testing_set_df['Item sets'] = testing_set_df['Item sets'].apply(lambda x: ast.literal_eval(x))
+testing_set = testing_set_df['Item sets']
 ############################################# AUXILIARY AND SINGLE SELECTION FUNCTIONS #######################################################
 def fitness_function(object_list): 
     fraction_util = 0
@@ -203,14 +197,14 @@ def single_djd(outer_bin_dict, outer_objects, problems, current_bin, BIN_WIDTH, 
     else:
         logging.info(f'Fatal error, item {i}:{item} larger than bin')
     return objects, bin_dict, items, current_object
-############################################# PARAMETERS #######################################################
+
 # GLOBAL (NEED TO CHANGES ACROSS MULTIPLE PROGRAMS)
 BIN_WIDTH = 100
 BIN_HEIGHT = 100
 NUM_RECTANGLES = 20
 
 # SPECIFIC (JUST FOR GENETIC ALGORITHM)
-population_size = 25
+testing_population_size = 5
 seeding = False 
 problems_per_round = 20
 elitism = 0.1 # Fraction which are transferred to the next generation unchanged, or mutation (mutated elitist)
@@ -218,122 +212,56 @@ reproduction_rate = 0.4 # Fraction which are chosen to reproduce
 rotation_chance = 0.1
 point_chance = 0.1
 num_iterations = 200
-############################################# GENETIC ALGORITHM #######################################################
+
+##################################################################################################################
+# GET POPULATION FROM FITNESS DATAFRAME
+population = pd.read_csv('archive data/popsize=40,num_iterations=200/fitness_df_150.csv').iloc[:testing_population_size]
+population['Chromosome'] = population['Chromosome'].apply(lambda x: ast.literal_eval(x))
+population = population['Chromosome']
+
+fitness_dict = {i:[] for i in range(testing_population_size)}
+
+count = 0
+for current_problem in testing_set:
+    print(f'Problem {count}')
+    count += 1
+    for chromosome_index in range(testing_population_size):
+        chromosome = population[chromosome_index]
+        problem = copy.deepcopy(current_problem)
+        bin_dict = {'0':[]}
+        objects =  [[np.zeros((BIN_HEIGHT, BIN_WIDTH)), None]]
+        round = 0
+        current_bin = 0
+        while round < NUM_RECTANGLES:
+            solution = chromosome[round]
+            if solution in [1,3,5]: # choose largest remaining piece by area
+                areas = [[x[0] * x[1], x] for x in problem]
+                areas.sort(reverse=True)
+                item = areas[0][1]
+                problem.remove(item)
+            elif solution in [0,2,4]:
+                item = problem.pop(0) # instead choose first piece in queue
+
+            if solution in [0,1]:
+                objects, bin_dict, current_bin = single_ff(bin_dict, objects, item, round, BIN_WIDTH, BIN_HEIGHT)
+
+            elif solution in [2,3]:
+                objects, bin_dict, current_bin = single_nf(bin_dict, objects, item, round, current_bin, BIN_WIDTH, BIN_HEIGHT)
+            
+            elif solution in [4,5]:
+                objects, bin_dict, current_bin = single_bf(bin_dict, objects, item, round, BIN_WIDTH, BIN_HEIGHT)
+
+            elif solution == 6:
+                objects, bin_dict, problem, current_bin = single_djd(bin_dict, objects, problem, current_bin, BIN_WIDTH, BIN_HEIGHT)
+
+            round += 1
+        fitness = fitness_function(objects)
+        fitness_dict[chromosome_index].append(fitness)
+
+fitness_df = pd.DataFrame(fitness_dict)
 
 
-# GENERATE INITIAL POPULATION
-if not seeding: # RANDOMLY
-    population = []
-    for i in range(population_size):
-        initial_chromosome = [seed.integers(0,7) for i in range(NUM_RECTANGLES)]
-        population.append(initial_chromosome)
-else:
-    pass
-
-# MAIN LOOP
-for iteration in range(num_iterations):
-    print(iteration)
-    # Assign [problems_per_round] problems to each chromosome and calculate fitness
-    fitness_dict = {i:[] for i in range(population_size)}
-    # Randomly sample a problem from the training set
-    for problem_index in range(problems_per_round):
-        current_problem = training_set[seed.integers(0,len(training_set))]
-
-        # Solve selected problem with each heuristic
-        for chromosome_index in range(population_size):
-            chromosome = population[chromosome_index]
-            problem = copy.deepcopy(current_problem)
-            bin_dict = {'0':[]}
-            objects =  [[np.zeros((BIN_HEIGHT, BIN_WIDTH)), None]]
-            round = 0
-            current_bin = 0
-            while round < NUM_RECTANGLES:
-                solution = chromosome[round]
-                if solution in [1,3,5]: # choose largest remaining piece by area
-                    areas = [[x[0] * x[1], x] for x in problem]
-                    areas.sort(reverse=True)
-                    item = areas[0][1]
-                    problem.remove(item)
-                elif solution in [0,2,4]:
-                    item = problem.pop(0) # instead choose first piece in queue
-
-                if solution in [0,1]:
-                    objects, bin_dict, current_bin = single_ff(bin_dict, objects, item, round, BIN_WIDTH, BIN_HEIGHT)
-
-                elif solution in [2,3]:
-                    objects, bin_dict, current_bin = single_nf(bin_dict, objects, item, round, current_bin, BIN_WIDTH, BIN_HEIGHT)
-                
-                elif solution in [4,5]:
-                    objects, bin_dict, current_bin = single_bf(bin_dict, objects, item, round, BIN_WIDTH, BIN_HEIGHT)
-
-                elif solution == 6:
-                    objects, bin_dict, problem, current_bin = single_djd(bin_dict, objects, problem, current_bin, BIN_WIDTH, BIN_HEIGHT)
-
-                round += 1
-            fitness = fitness_function(objects)
-            fitness_dict[chromosome_index].append(fitness)
-
-    # Store and sort fitnesses
-    for i in fitness_dict:
-        logging.info(f'{i}: {fitness_dict[i]}, {len(fitness_dict[i])}')
-    fitness_series = pd.DataFrame(fitness_dict).describe().loc['50%']
-    fitness_df = pd.DataFrame({'Chromosome':[chromosome for chromosome in population], 'Fitness':fitness_series}).sort_values(by='Fitness', ascending=False)
-
-    if iteration % 20 == 0:
-        fitness_df.to_csv(f'fitness_df_{iteration}.csv')
-
-    child_population = list(fitness_df['Chromosome'].iloc[:math.floor(elitism*population_size)])
-    # PARENT SELECTION: Roulette wheel 
-    total_fitness = fitness_df['Fitness'].sum()
-    normalised_fitness_series = fitness_df['Fitness'].div(total_fitness)
-    roulette_wheel = normalised_fitness_series.cumsum().to_dict()
-    parents = []
-
-    roulette_results = seed.random(2*math.ceil(population_size*reproduction_rate))
-    for result in roulette_results:
-        for slice in roulette_wheel:
-            if result < roulette_wheel[slice]:
-                parents.append(slice)
-                break
-
-    parents_indices = np.split(np.array(parents), math.ceil(population_size * reproduction_rate))
-
-    # CROSSOVER - 2 point selection (maybe partially mapped?)
-    for child_index in range(math.ceil(population_size * reproduction_rate)):
-        parent1 = population[parents_indices[child_index][0]]
-        parent2 = population[parents_indices[child_index][1]]
-
-        crossover_points = seed.integers(0,NUM_RECTANGLES,2)
-        crossover_point1 = min(crossover_points)
-        crossover_point2 = max(crossover_points)
-        
-        child1 = parent1[:crossover_point1] + parent2[crossover_point1:crossover_point2] + parent1[crossover_point2:]
-        child2 = parent2[:crossover_point1] + parent1[crossover_point1:crossover_point2] + parent2[crossover_point2:]
-        child_population.append(child1)
-        child_population.append(child2)
-
-    # MUTATION - rotation (most likely), swapping individuals(least likely)
-    for child_index in range(len(child_population)):
-        chance = seed.random()
-
-        # rotation mutation - changes order of heuristics
-        if chance <= rotation_chance:
-            rotation_size = seed.choice([1,2,3])
-            child_population[child_index] = list(np.roll(child_population[child_index],rotation_size))
-
-        # point mutation - changes a heuristic
-        if chance <= point_chance:
-            random_index = seed.integers(NUM_RECTANGLES)
-            child_population[child_index][random_index] = seed.integers(0,7)
-
-
-    # elitism: best [elitism] percent of chromosomes are carried over to the next generation
-    elites = list(fitness_df['Chromosome'].iloc[:math.ceil(elitism*population_size)])
-    for elite in elites:
-        child_population.append(elite)
-    
-    population = child_population
-
-
-new_population = pd.DataFrame({'Chromosome':[chromosome for chromosome in population]})
-new_population.to_csv('new_population.csv')
+output_df = pd.concat([testing_set_df,fitness_df], axis=1)
+output_df.drop(columns=['Unnamed: 0'], inplace=True)
+output_df.to_csv('hyper_heuristic_testing_archive data_popsize=40,num_iterations=200_fitness_df_150.csv.csv')
+output_df.describe().to_csv(f'hyper_heuristic_testing_summary_archive data_popsize=40,num_iterations=200_fitness_df_150.csv.csv')
